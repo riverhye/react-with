493b6d534748db33a8f571ff8ad17dfe4a00c2d8
riverhye <uhzigani@gmail.com>
2024-01-20 17:08:35 +0900

    ✨ App.js에 router 적용

--- tailwind/pra2/src/App.js
+++ tailwind/pra2/src/App.js
@@ -1,11 +1,20 @@
-import CatCard from './components/CatCard.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './components/pages/Main';
+
+// import tailwindcss
 import './styles/tailwind.css';
+import CatDetail from './components/pages/CatDetail';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <CatCard />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/post/:id" element={<CatDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
