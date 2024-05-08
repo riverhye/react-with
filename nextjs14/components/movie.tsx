4e26976dfe4e9bbc05832c67f49841a4c8aebc90
riverhye <uhzigani@gmail.com>
2024-05-08 16:03:13 +0900

    ✨ onClick event

--- nextjs14/components/movie.tsx
+++ nextjs14/components/movie.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import styles from '../styles/movie.module.css';
+import { useRouter } from 'next/navigation';
 
 interface IMovieProps {
   id: string;
@@ -8,9 +11,14 @@ interface IMovieProps {
 }
 
 export default function Movie({ id, title, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+
   return (
     <div id={id} className={styles.movie}>
-      <img src={poster_path} alt={title} />
+      <img src={poster_path} alt={title} onClick={onClick} />
       <Link href={`/movies/${id}`}>{title}</Link>
     </div>
   );
